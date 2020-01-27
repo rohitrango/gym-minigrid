90c1d7f758f784764be8d236453f20fe34fb82db
Vidhi Jain <vidhiJain@users.noreply.github.com>
2020-01-27 17:56:13 -0500

    Update __init__.py

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -19,10 +19,10 @@ from gym_minigrid.envs.crossing import *
 from gym_minigrid.envs.lavagap import *
 from gym_minigrid.envs.dynamicobstacles import *
 from gym_minigrid.envs.distshift import *
-<<<<<<< HEAD
+
 from gym_minigrid.envs.hallwithvictims import *
 from gym_minigrid.envs.SARplayground import * 
-=======
+
 from gym_minigrid.envs.simpleroom import *
 from gym_minigrid.envs.multiobj import *
->>>>>>> b771053a1e90e5dc78fb29826eefc21f8e13a0d6
+
