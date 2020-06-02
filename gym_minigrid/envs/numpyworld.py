8786a59b8bab124aea0de96ab35d424a8f76cfb0
rohitrango <rohit.rango@gmail.com>
2020-06-02 11:42:27 -0400

    added victimcount

--- gym_minigrid/envs/numpyworld.py
+++ gym_minigrid/envs/numpyworld.py
@@ -9,66 +9,6 @@ from gym_minigrid import preprocessing
 RESOURCES_DIR = (Path(__file__).parent / './resources').resolve()
 
 
-class NumpyMapFourRooms(MiniGridEnv):
-    """
-    Environment with multiple rooms and random objects.
-    This environment has no specific goals or rewards.
-    """
-
-    def __init__(self, numpyFile=Path(RESOURCES_DIR, 'map000.npy')):
-        self.numpyFile = numpyFile
-        self.index_mapping = {
-             0 : 'unseen'        ,
-             1 : 'empty'         ,
-             2 : 'wall'          ,
-             3 : 'floor'         ,
-             4 : 'door'          ,
-             5 : 'key'           ,
-             6 : 'ball'          ,
-             7 : 'box'           ,
-             8 : 'goal'          ,
-             9 : 'lava'          ,
-             10: 'agent'
-        }
-        super().__init__(grid_size=41, max_steps=1000, agent_view_size=3)
-
-
-    def _gen_grid(self, width, height):
-
-        # Create an empty grid
-        self.grid = Grid(width, height)
-
-        # Generate the surrounding walls
-        self.grid.wall_rect(0, 0, width, height)
-
-        # Create the grid
-        self.array = np.load(self.numpyFile)
-
-        for i in range(1, self.array.shape[0]):
-            for j in range(1, self.array.shape[1]):
-                entity_name = index_mapping[self.array[i][j]]
-                entity_index = int(self.array[i][j])
-
-                if entity_index != 10 and entity_index != 0:  #'agent':
-                    for entity_class in WorldObj.__subclasses__():
-                        # the class name needs to be lowercase (not sentence case)
-                        if entity_name == entity_class.__name__.casefold():
-                            #     print('entity_index')
-
-                            self.put_obj(entity_class(), j, i)
-
-                elif entity_index == 0:
-                    self.put_obj(Wall(), j, i)
-        self.place_agent()
-        self.place_obj(Goal())
-        self.mission = 'Reach the goal'
-
-
-    def step(self, action):
-        obs, reward, done, info = MiniGridEnv.step(self, action)
-        return obs, reward, done, info
-
-
 class NumpyMapMinecraftUSAR(MiniGridEnv):
 
     def __init__(self, numpyFile=Path(RESOURCES_DIR, 'tmp_grid.npy'), agent_start_pos=[24, 25], agent_start_dir=2):
@@ -133,6 +73,7 @@ class NumpyMapMinecraftUSAR(MiniGridEnv):
 
         # Create an empty grid
         self.grid = Grid(width, height)
+        self.victimcount = 0
 
         # Create the grid
         self.array = np.load(self.numpyFile)
@@ -162,9 +103,12 @@ class NumpyMapMinecraftUSAR(MiniGridEnv):
                         if entity_name == entity_class.__name__.casefold():
                             # print(entity_index, entity_name, entity_color, entity_toggletime)
                             if entity_color != '':
+                                if entity_class == Goal and entity_color in ['yellow', 'green']:
+                                    self.victimcount += 1
                                 self.put_obj(entity_class(color=entity_color), mg_j, mg_i)
                             else:
                                 if entity_class == Goal:
+                                    self.victimcount += 1
                                     self.put_obj(entity_class(color=np.random.choice(['yellow', 'green'])), mg_j, mg_i)
                                 else:
                                     self.put_obj(entity_class(), mg_j, mg_i)
@@ -282,11 +226,100 @@ class NumpyMapMinecraftUSAR(MiniGridEnv):
                 self.put_obj(Goal('white'), self.front_pos[0], self.front_pos[1])
                 self.array[self.front_pos[1], self.front_pos[0]] = 9
                 done = False
+                # Check for total victim count
+                self.victimcount -= 1
+                if self.victimcount == 0:
+                    done = True
+
             elif cur_cell != None and cur_cell.type == 'goal':
                 done = False
         return obs, reward, done, info
 
 
+##################################
+# This is for curriculum
+##################################
+class USARLevel1(NumpyMapMinecraftUSAR):
+    def __init__(self, *args, **kwargs):
+        self.currfile = Path(RESOURCES_DIR, 'tmp_grid_curriculum.npy')
+        self.curriculumlvls = list(np.load(self.currfile))
+        super().__init__(*args, **kwargs)
+
+
+    def _gen_grid(self, width, height):
+        # Keep victim count
+        self.victimcount = 0
+        # Load the array
+        self.array = np.load(self.numpyFile)
+        self.array = preprocessing.preprocess_connected_components(self.array, self.index_mapping)
+        lvlidx = np.random.randint(len(self.curriculumlvls))
+        x1, y1, x2, y2 = self.curriculumlvls[lvlidx]
+        w = x2 - x1 + 1
+        h = y2 - y1 + 1
+        # Get actual width and height
+        width, height = 2+max(w,h), 2+max(w,h)
+        self.width = width
+        self.height = height
+
+        # Set subarray
+        subarray = np.zeros((height, width)) + 4
+        subarray[1:1+h, 1:1+w] = self.array[y1:y2+1, x1:x2+1]
+        self.array = subarray
+
+        # Create an empty grid
+        self.grid = Grid(width, height)
+        # Save a wallgrid and doorgrid
+        self.wallmap = self._get_filtered_map('wall') + self._get_filtered_map('unseen')
+        self.doormap = self._get_filtered_map('door')
+
+        # Put entities in the grid
+        # Keep track of empty positions to keep agent in it
+        emptypos = []
+        for mc_i in range(0, self.array.shape[0]):
+            for mc_j in range(0, self.array.shape[1] ):
+                mg_i , mg_j = mc_i , mc_j
+                entity_index = int(self.array[mc_i][mc_j])
+
+                entity_name = self.index_mapping[entity_index]
+                entity_color = self.color_mapping[entity_index]
+                entity_toggletime = self.toggletimes_mapping[entity_index]
+
+                if entity_name in ['agent', 'empty']:
+                    emptypos.append([mg_j, mg_i])
+                    continue
+                elif entity_name == 'unseen':
+                    self.put_obj(Wall(), mg_j, mg_i)
+                else:
+                    for entity_class in WorldObj.__subclasses__():
+                        # the class name needs to be lowercase (not sentence case)
+                        if entity_name == entity_class.__name__.casefold():
+                            # print(entity_index, entity_name, entity_color, entity_toggletime)
+                            if entity_color != '' and entity_class != Goal:
+                                self.put_obj(entity_class(color=entity_color), mg_j, mg_i)
+                            else:
+                                if entity_class == Goal:
+                                    self.victimcount += 1
+                                    self.put_obj(entity_class(color=np.random.choice(['yellow', 'green'])), mg_j, mg_i)
+                                else:
+                                    self.put_obj(entity_class(), mg_j, mg_i)
+
+        # Set agent position and directions
+        self.agent_start_pos = emptypos[np.random.randint(len(emptypos))]
+        self.agent_pos = self.agent_start_pos
+        self.grid.set(*self.agent_start_pos, None)
+        self.agent_dir = self.agent_start_dir
+        # self.place_obj(Goal())
+        self.mission = 'Triage the victims'
+
+        # Load room information for easy querying later on
+        #self.roomViews = self._get_door_to_room_mapping(np.load(self.roomFile))
+        self.roomViews = dict()
+
+
+
+#####################################################
+# Random victims locations designed by Ini
+#####################################################
 
 class NumpyMapMinecraftUSARRandomVictims(NumpyMapMinecraftUSAR):
     def __init__(self, num_victims_red=10, num_victims_green=20):
@@ -353,6 +386,11 @@ register(
 register(
     id='MiniGrid-NumpyMapMinecraftUSAR-v0',
     entry_point='gym_minigrid.envs:NumpyMapMinecraftUSAR'
+)
+
+register(
+    id='MiniGrid-NumpyMapMinecraftUSARlvl1-v0',
+    entry_point='gym_minigrid.envs:USARLevel1'
 )
 
 register(
